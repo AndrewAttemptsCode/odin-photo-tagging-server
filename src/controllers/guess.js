698248b917e63d6e0efe37b7f3db390343e15f11
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-25 15:54:32 +0100

    feat: add status message for json response if correct or wrong guess

--- src/controllers/guess.js
+++ src/controllers/guess.js
@@ -17,9 +17,9 @@ const checkGuess = asyncHandler(async (req, res) => {
     Math.abs(coordY - item.coordY) < item.deviation
   ) {
     console.log(`You got the ${item.name}`);
-    return res.json({ message: `You got the ${item.name}` });
+    return res.json({ status: "success", message: `You got the ${item.name}` });
   } else {
-    return res.status(404).json({ message: `${item.name} not found` });
+    return res.status(404).json({ status: "fail", message: `${item.name} not found` });
   }
 });
 
